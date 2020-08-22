0051d619faf0eaf4a517c8a01bdb52465ec34bd6
Liz P <parisielizabeth@gmail.com>
2020-08-22 02:43:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
 // Your code here
+
+function createEmployeeRecord(employees){
+  return {
+    firstName: employees[0],
+    familyName: employees[1],
+    title: employees[2],
+    payPerHour: employees[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
+}
+
+function createEmployeeRecords(arrayofEmployees){
+  return arrayofEmployees.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(record, timeIn){
+  let [date, hour] = timeIn.split(' ');
+  
+  record.timeInEvents.push({
+    type: "TimeIn", 
+    hour: parseInt(hour, 10),
+    date: date, 
+  });
+    return record;
+}
+
+function createTimeOutEvent(record, timeOut){
+  let [date, hour] = timeOut.split(' ');
+  
+  record.timeOutEvents.push({
+    type: "TimeOut", 
+    hour: parseInt(hour, 10),
+    date: date, 
+  });
+    return record;
+}
+
+function hoursWorkedOnDate(record, dateWorked){
+  let clockIn = record.timeInEvents.find(event => event.date === dateWorked);
+  let clockOut = record.timeOutEvents.find(event => event.date === dateWorked);
+  
+  return (clockOut.hour - clockIn.hour) / 100;
+}
+
+function wagesEarnedonDate(record, dateWorked){
+  let hours = hoursWorkedOnDate(record, dateWorked);
+  return hours * record.payPerHour;
+}
+
+function allWagesFor(record){
+  
+}
+
+function findEmployeeByFirstName(records, firstName){
+  return records.find(record => {
+    return record.firstName === firstName;
+  });
+}
+
+function calculatePayroll(arrayofEmployees){
+
+}
