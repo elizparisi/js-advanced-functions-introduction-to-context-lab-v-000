c251013611099140c653e002a54c53fc8be961a1
Liz P <parisielizabeth@gmail.com>
2020-08-22 04:19:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function wagesEarnedOnDate(record, dateWorked){
 }
 
 function allWagesFor(record){
-  return record.timeInEvents.map(event => wagesEarnedOnDate(employeeRecord, event.date)).reduce((total, hoursWorked) => total + hoursWorked);
+  return record.timeInEvents.map(event => wagesEarnedOnDate(employeeRecord, event.date)).reduce((total, hours) => total + hours);
 }
 
 function findEmployeeByFirstName(records, firstName){
